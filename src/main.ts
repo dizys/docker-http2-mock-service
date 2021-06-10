947b634d9b057825cf7f43950370d726d45377a9
Dizy <dev@dizy.cc>
2021-06-10 17:49:17 +0800

    Non-ssl attempt

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import HTTP2 from 'http2';
 import Koa from 'koa';
 
 import {config} from './config';
-import {key, cert} from './keys';
+// import {key, cert} from './keys';
 import {router} from './router';
 
 const app = new Koa();
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const server = HTTP2.createSecureServer(
-  {key, cert, allowHTTP1: true},
-  app.callback(),
-);
+// const server = HTTP2.createSecureServer(
+//   {key, cert, allowHTTP1: true},
+//   app.callback(),
+// );
+
+const server = HTTP2.createServer(app.callback());
 
 server.listen(config.port, () => {
   console.info(`HTTP2 mock service listening on port: ${config.port}...`);
