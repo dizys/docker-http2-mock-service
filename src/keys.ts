a6de727fb9819afe395e187b304b43f18e24ed43
Dizy <dev@dizy.cc>
2021-06-10 17:56:29 +0800

    Support non-ssl server

--- src/keys.ts
+++ src/keys.ts
@@ -5,38 +5,45 @@ import {
   PROJECT_SERVER_CERT_PATH,
   PROJECT_SERVER_KEY_PATH,
 } from './path';
-import {camelToSnakeCase, fatalError} from './utils';
 
-let keyPath;
-
-if (FS.existsSync(PROJECT_SERVER_KEY_PATH)) {
-  keyPath = PROJECT_SERVER_KEY_PATH;
-} else if (FS.existsSync(GLOBAL_SERVER_KEY_PATH)) {
-  keyPath = GLOBAL_SERVER_KEY_PATH;
-} else {
-  fatalError('Fatal Error: SSL key file not found');
+export interface Keys {
+  key: string;
+  cert: string;
 }
 
-let certPath;
+export function getKeys(): Keys | undefined {
+  let keyPath;
 
-if (FS.existsSync(PROJECT_SERVER_CERT_PATH)) {
-  certPath = PROJECT_SERVER_CERT_PATH;
-} else if (FS.existsSync(GLOBAL_SERVER_CERT_PATH)) {
-  certPath = GLOBAL_SERVER_CERT_PATH;
-} else {
-  fatalError('Fatal Error: SSL cert file not found');
+  if (FS.existsSync(PROJECT_SERVER_KEY_PATH)) {
+    keyPath = PROJECT_SERVER_KEY_PATH;
+  } else if (FS.existsSync(GLOBAL_SERVER_KEY_PATH)) {
+    keyPath = GLOBAL_SERVER_KEY_PATH;
+  } else {
+    return undefined;
+  }
+
+  let certPath;
+
+  if (FS.existsSync(PROJECT_SERVER_CERT_PATH)) {
+    certPath = PROJECT_SERVER_CERT_PATH;
+  } else if (FS.existsSync(GLOBAL_SERVER_CERT_PATH)) {
+    certPath = GLOBAL_SERVER_CERT_PATH;
+  } else {
+    return undefined;
+  }
+
+  let key: string;
+  let cert: string;
+
+  try {
+    key = FS.readFileSync(keyPath, {encoding: 'utf8'});
+    cert = FS.readFileSync(certPath, {encoding: 'utf8'});
+    console.log(`Using SSL key from: ${keyPath}...`);
+    console.log(`Using SSL cert from: ${certPath}...`);
+  } catch (error) {
+    console.log(`Failed to read keys: ${String(error)}`);
+    return undefined;
+  }
+
+  return {key, cert};
 }
-
-let key: string;
-let cert: string;
-
-try {
-  key = FS.readFileSync(keyPath, {encoding: 'utf8'});
-  cert = FS.readFileSync(certPath, {encoding: 'utf8'});
-  console.log(`Using SSL key from: ${keyPath}...`);
-  console.log(`Using SSL cert from: ${certPath}...`);
-} catch (error) {
-  fatalError(`Failed to read keys: ${String(error)}`);
-}
-
-export {key, cert};
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
-import HTTP2 from 'http2';
+import HTTP2, {Http2SecureServer, Http2Server} from 'http2';
 import Koa from 'koa';
 
 import {config} from './config';
-// import {key, cert} from './keys';
+import {getKeys} from './keys';
 import {router} from './router';
 
 const app = new Koa();
 
 app.use(router.routes()).use(router.allowedMethods());
 
-// const server = HTTP2.createSecureServer(
-//   {key, cert, allowHTTP1: true},
-//   app.callback(),
-// );
+let keys = getKeys();
 
-const server = HTTP2.createServer(app.callback());
+let server: Http2Server | Http2SecureServer;
+
+if (keys) {
+  server = HTTP2.createSecureServer(
+    {...keys, allowHTTP1: true},
+    app.callback(),
+  );
+} else {
+  console.log(`No SSL keys, creating non-ssl http2 server...`);
+  server = HTTP2.createServer(app.callback());
+}
 
 server.listen(config.port, () => {
   console.info(`HTTP2 mock service listening on port: ${config.port}...`);
